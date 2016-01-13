29e8b90992f7334c4cce6ad54df2c187cd22eed5
Marcus Jaschen <mjaschen@gmail.com>
2016-01-13 15:08:26 +0100

    feat: add PHP League CSV as alternative CSV generator
    - add CSV generator class
    - add tests for CSV generator

---
+++ src/Csv/LeagueCsvGenerator.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * CSV Generator Class
+ *
+ * @category  Collmex
+ * @author    Marcus Jaschen <mail@marcusjaschen.de>
+ * @copyright 2013 Marcus Jaschen
+ * @license   http://www.opensource.org/licenses/mit-license MIT License
+ * @link      http://github.com/mjaschen/collmex
+ */
+
+namespace MarcusJaschen\Collmex\Csv;
+
+use League\Csv\Writer;
+
+/**
+ * CSV Generator Class
+ *
+ * @category Collmex
+ * @author   Marcus Jaschen <mail@marcusjaschen.de>
+ * @license  http://www.opensource.org/licenses/mit-license MIT License
+ * @link     http://github.com/mjaschen/collmex
+ */
+class LeagueCsvGenerator implements GeneratorInterface
+{
+    /**
+     * @var string
+     */
+    protected $delimiter;
+
+    /**
+     * @var string
+     */
+    protected $enclosure;
+
+    /**
+     * @param string $delimiter
+     * @param string $enclosure
+     */
+    public function __construct($delimiter = ';', $enclosure = '"')
+    {
+        $this->delimiter = $delimiter;
+        $this->enclosure = $enclosure;
+    }
+
+    /**
+     * Generates a CSV string from given array data
+     *
+     * @param array $data
+     *
+     * @throws \RuntimeException
+     *
+     * @return string
+     */
+    public function generate(array $data)
+    {
+        $writer = Writer::createFromString('');
+        $writer->setDelimiter($this->delimiter);
+        $writer->setEnclosure($this->enclosure);
+
+        if (! is_array($data[0])) {
+            $data = [$data];
+        }
+
+        $writer->insertAll($data);
+
+        return $writer->__toString();
+    }
+}
---
+++ tests/Csv/LeagueCsvGeneratorTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace MarcusJaschen\Collmex\Csv;
+
+class LeagueCsvGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SimpleGenerator
+     */
+    protected $generator;
+
+    public function setUp()
+    {
+        $this->generator = new LeagueCsvGenerator(';', '"');
+    }
+
+    public function tearDown()
+    {
+        unset($this->generator);
+    }
+
+    public function testGenerateCsvOneLine()
+    {
+        $data = [
+            [
+                'MESSAGE',
+                'E',
+                '11111',
+                'Error Message',
+                '123',
+            ],
+        ];
+
+        $expected = 'MESSAGE;E;11111;"Error Message";123' . "\n";
+
+        $this->assertEquals($expected, $this->generator->generate($data));
+    }
+
+    public function testGenerateCsvMultipleLines()
+    {
+        $data = [
+            [
+                'TEST',
+                '1',
+                'a',
+            ],
+            [
+                'MESSAGE',
+                'E',
+                '11111',
+                'Error Message',
+                '123',
+            ],
+        ];
+
+        $expected = 'TEST;1;a' . "\n"
+                    . 'MESSAGE;E;11111;"Error Message";123' . "\n";
+
+        $this->assertEquals($expected, $this->generator->generate($data));
+    }
+
+    public function testGenerateCsvOneLineWithoutOuterArray()
+    {
+        $data = [
+            'MESSAGE',
+            'E',
+            '11111',
+            'Error Message',
+            '123',
+        ];
+
+        $expected = 'MESSAGE;E;11111;"Error Message";123' . "\n";
+
+        $this->assertEquals($expected, $this->generator->generate($data));
+    }
+
+
+    /**
+     * Tests if CSV generation works in a special case: text contains a
+     * backslash followed by a double quote. There exists a PHP bug where
+     * fputcsv() creates an invalid CSV string in this case.
+     *
+     * @see https://bugs.php.net/bug.php?id=43225
+     */
+    public function testGenerateCsvWithSpecialCharactersWorksAsExpected()
+    {
+        $this->markTestSkipped('Skipping test for PHP bug 43225');
+        return;
+
+        $data = [
+            'CMXINV',
+            '-1001338',
+            'Provision Bikemarkt-Verkauf 279679: "Endura MTR Baggy Short // SALE \\\\" an "bebetz" am 1.1.2016',
+        ];
+
+        $expected = 'CMXINV;-1001338;"Provision Bikemarkt-Verkauf 279679: ""Endura MTR Baggy Short // SALE \\\\"" an ""bebetz"" am 1.1.2016"' . PHP_EOL;
+
+        $this->assertEquals($expected, $this->generator->generate($data));
+    }
+}
