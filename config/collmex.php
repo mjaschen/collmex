874367841ef5d7356a2b88fa8f7369e5476f368f
Naoray Kroaths <krishan.koenig@googlemail.com>
2017-12-19 14:55:11 +0100

    remove unused cliend_it from collmex config

--- config/collmex.php
+++ config/collmex.php
@@ -5,7 +5,6 @@
  * Used by CollmexServiceProvider when running under Laravel 4.
  */
 return [
-    'client_id' => env('COLLMEX_CLIENT_ID', ''),
     'user'      => env('COLLMEX_USER', ''),
     'password'  => env('COLLMEX_PASSWORD', ''),
     'customer'  => env('COLLMEX_CUSTOMER_ID', ''),
