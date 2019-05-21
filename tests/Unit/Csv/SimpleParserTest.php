1b195e3e37e81e306f6f7fe247047f245d1708d2
Oliver Klee <github@oliverklee.de>
2019-05-21 20:36:12 +0200

    [TASK] Rework the simple CSV parser test
    Minify the tests and add tests for some special conditions.
    
    The LeageCsvParser test is not changed as that class will be removed soon.

--- tests/Unit/Csv/SimpleParserTest.php
+++ tests/Unit/Csv/SimpleParserTest.php
@@ -3,82 +3,129 @@ declare(strict_types=1);
 
 namespace MarcusJaschen\Collmex\Tests\Unit\Csv;
 
+use MarcusJaschen\Collmex\Csv\SimpleParser;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test case.
+ */
 class SimpleParserTest extends TestCase
 {
     /**
-     * @var \MarcusJaschen\Collmex\Csv\SimpleParser
+     * @var SimpleParser
      */
-    protected $parser;
+    protected $parser = null;
 
     protected function setUp(): void
     {
-        $this->parser = new \MarcusJaschen\Collmex\Csv\SimpleParser(';', '"');
+        $this->parser = new SimpleParser();
     }
 
     /**
      * @test
      */
-    public function parseOneLine(): void
+    public function parseWithEmptyStringReturnsEmptyArray(): void
     {
-        $csv = 'Typkennung;Rechnung Nr;Pos';
+        $result = $this->parser->parse('');
 
-        $data = $this->parser->parse($csv);
-
-        $expected = [
-            [
-                'Typkennung',
-                'Rechnung Nr',
-                'Pos',
-            ],
-        ];
-
-        self::assertSame($expected, $data);
+        self::assertSame([], $result);
     }
 
     /**
      * @test
      */
-    public function parseOneLineWithNewline(): void
+    public function parseWithOneCellReturnsThatCell(): void
     {
-        $csv = "Typkennung;Rechnung Nr;Pos\n";
+        $cellContents = 'Hello world!';
 
-        $data = $this->parser->parse($csv);
+        $result = $this->parser->parse($cellContents);
 
-        $expected = [
-            [
-                'Typkennung',
-                'Rechnung Nr',
-                'Pos',
-            ],
-        ];
-
-        self::assertSame($expected, $data);
+        self::assertSame([[$cellContents]], $result);
     }
 
     /**
      * @test
      */
-    public function parseMultipleLines(): void
+    public function parseSeparatesColumnsWithGivenSeparator(): void
     {
-        $csv = "Typkennung;Rechnung Nr;Pos\nCMXINV;100;1";
+        $separator = ',';
+        $cellContents1 = 'Hello';
+        $cellContents2 = 'world';
+        $csv = $cellContents1 . $separator . $cellContents2;
+        $subject = new SimpleParser($separator, '"');
 
-        $data = $this->parser->parse($csv);
+        $result = $subject->parse($csv);
 
-        $expected = [
-            [
-                'Typkennung',
-                'Rechnung Nr',
-                'Pos',
-            ],
-            [
-                'CMXINV',
-                '100',
-                '1',
-            ],
-        ];
+        self::assertSame([[$cellContents1, $cellContents2]], $result);
+    }
 
-        self::assertSame($expected, $data);
+    /**
+     * @test
+     */
+    public function parseByDefaultSeparatesColumnsWithSemicolon(): void
+    {
+        $separator = ';';
+        $cellContents1 = 'Hello';
+        $cellContents2 = 'world';
+        $csv = $cellContents1 . $separator . $cellContents2;
+
+        $result = $this->parser->parse($csv);
+
+        self::assertSame([[$cellContents1, $cellContents2]], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function parseByDefaultUsesDoubleQuotesAsEnclosure(): void
+    {
+        $cellContents = "Hello\nworld";
+        $csv = '"' . $cellContents . '"';
+
+        $result = $this->parser->parse($csv);
+
+        self::assertSame([[$cellContents]], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function doubleEnclosureEscapesIt(): void
+    {
+        $enclosure = '!';
+        $cellContents = $enclosure . $enclosure;
+        $csv = $enclosure . $cellContents . $enclosure;
+        $subject = new SimpleParser(';', $enclosure);
+
+        $result = $subject->parse($csv);
+
+        self::assertSame([[$enclosure]], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function parseReturnsLinesSeparatedByLinefeed(): void
+    {
+        $line1 = 'There is no spoon.';
+        $line2 = 'The cake is a lie';
+        $csv = $line1 . "\n" . $line2;
+
+        $result = $this->parser->parse($csv);
+
+        self::assertSame([[$line1], [$line2]], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function parseIgnoresOneTrailingLinefeed(): void
+    {
+        $cellContents = 'Hello world!';
+        $csv = $cellContents . "\n";
+
+        $result = $this->parser->parse($csv);
+
+        self::assertSame([[$cellContents]], $result);
     }
 }
