059a56be4feef8e96286d860143f772d90e60bc8
Marcus Jaschen <mjaschen@gmail.com>
2019-09-02 15:19:24 +0200

    update doc block as suggested in code review

--- src/Response/Exception/InvalidZipResponseException.php
+++ src/Response/Exception/InvalidZipResponseException.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace MarcusJaschen\Collmex\Response\Exception;
 
 /**
- * This exception is thrown if an invalid Zip response is
- * detected, i. e. when a Zip Response doesn't contain
+ * This exception is thrown if an invalid ZIP response is
+ * detected, i.e., when a ZIP Response does not contain
  * its CSV counterpart.
  */
 class InvalidZipResponseException extends \RuntimeException
