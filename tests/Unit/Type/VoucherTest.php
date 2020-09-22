c9a534cb3e323f56e82cac1975d788505adfd5bb
Marcus Jaschen <mjaschen@gmail.com>
2020-09-22 15:28:28 +0200

    re-order imports

--- tests/Unit/Type/VoucherTest.php
+++ tests/Unit/Type/VoucherTest.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace MarcusJaschen\Collmex\Tests\Unit\Type;
 
 use MarcusJaschen\Collmex\Type\AbstractType;
-use MarcusJaschen\Collmex\Type\Voucher;
 use MarcusJaschen\Collmex\Type\TypeInterface;
+use MarcusJaschen\Collmex\Type\Voucher;
 use PHPUnit\Framework\TestCase;
 
 class VoucherTest extends TestCase
