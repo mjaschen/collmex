542c34409ff060dbc423f99422475d0c5ed552f4
Timo Paul <mail@timopaul.biz>
2020-12-07 11:27:16 +0100

    RequestContainer introduced
    send several requests at the same time with the RequestContainer

---
+++ src/RequestInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarcusJaschen\Collmex;
+
+/**
+ * Collmex Request Interface.
+ *
+ * @author   Timo Paul <mail@timopaul.biz>
+ */
+interface RequestInterface
+{
+  // 
+}
