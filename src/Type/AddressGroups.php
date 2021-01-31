a9a3c4bc7621affe7b5c7d8259232705789b336c
Johnny99211 <43766368+Johnny99211@users.noreply.github.com>
2021-02-01 00:52:59 +0100

    Create AddressGroups.php

---
+++ src/Type/AddressGroups.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarcusJaschen\Collmex\Type;
+
+/**
+ * @author   Marcus Jaschen <mail@marcusjaschen.de>
+ *
+ * @property string $type_identifier
+ * @property string $address_group_id
+ * @property string $description
+ */
+class AddressGroups extends AbstractType implements TypeInterface
+{
+    /**
+     * @var array
+     */
+	protected $template = [
+		'type_identifier'  => 'ADRGRP',
+		'address_group_id' => null,
+		'description'      => null
+	];
+
+    /**
+     * Formally validates the type data in $data attribute.
+     *
+     * @return bool Validation success
+     */
+    public function validate(): bool
+    {
+        return true;
+    }
+}
