6b49fe0fa9d0d5dcf0ff7917d472c3792793797f
Johnny99211 <43766368+Johnny99211@users.noreply.github.com>
2021-02-18 22:24:36 +0100

    Create InvoiceOutput.php
    Adding new type

---
+++ src/Type/InvoiceOutput.php
@@ -0,0 +1,102 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MarcusJaschen\Collmex\Type;
+
+/**
+ * Collmex Invoice Output Type.
+ *
+ * @author   Marcus Jaschen <mail@marcusjaschen.de>
+ *
+ * @property $type_identifier
+ * @property $client_id
+ * @property $invoice_id
+ * @property $output_medium
+ * @property $output_required
+ * @property $customer_id
+ * @property $invoice_date_begin
+ * @property $invoice_date_end
+ * @property $max_invoices_amount
+ * @property $also_deleted
+ * @property $dont_book
+ */
+class InvoiceOutput extends AbstractType implements TypeInterface {
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_MEDIUM_PRINT = 0;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_MEDIUM_EMAIL = 1;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_MEDIUM_FAX = 2;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_MEDIUM_MAIL = 3;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_MEDIUM_EMAIL_SIGNED = 4;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_MEDIUM_NO_OUTPUT = 100;
+	/**
+	 * @var int
+	 */
+	public const ALSO_DELETED_NO_DELETED = 0;
+	/**
+	 * @var int
+	 */
+	public const ALSO_DELETED_DELETED = 1;
+	/**
+	 * @var int
+	 */
+	public const DONT_BOOK_BOOK = 0;
+	/**
+	 * @var int
+	 */
+	public const DONT_BOOK_NO_BOOK = 1;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_REQUIRED_REQUIRED = 1;
+	/**
+	 * @var int
+	 */
+	public const OUTPUT_REQUIRED_NOT_REQUIRED = 0;
+
+	/**
+	 * @var array
+	 */
+	protected $template = [
+		// 1
+		'type_identifier'     => 'INVOICE_OUTPUT',
+		'client_id'           => null,
+		'invoice_id'          => null,
+		'output_medium'       => null,
+		// 5
+		'output_required'     => null,
+		'customer_id'         => null,
+		'invoice_date_begin'  => null,
+		'invoice_date_end'    => null,
+		'max_invoices_amount' => null,
+		// 10
+		'also_deleted'        => null,
+		'dont_book'           => null,
+	];
+
+	/**
+	 * Formally validates the type data in $data attribute.
+	 *
+	 * @return bool Validation success
+	 */
+	public function validate(): bool {
+		return true;
+	}
+}
