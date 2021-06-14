7fc45f0b80879a38c25747edca81ca3dfc687e61
Johnny99211 <43766368+Johnny99211@users.noreply.github.com>
2021-06-14 18:43:52 +0200

    Update InvoiceGet.php
    Add new fields to InvoiceGet. See http://www.collmex.de/c.cmx?1005,1,help,api_Rechnungen

--- src/Type/InvoiceGet.php
+++ src/Type/InvoiceGet.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace MarcusJaschen\Collmex\Type;
 
@@ -22,53 +22,55 @@ namespace MarcusJaschen\Collmex\Type;
  * @property mixed $created_by_system_only
  * @property mixed $stationary_exclude
  * @property mixed $output_required
+ * @property mixed $order_id
+ * @property mixed $product_id
  */
-class InvoiceGet extends AbstractType implements TypeInterface
-{
-    /**
-     * @var int
-     */
-    public const FORMAT_CSV = 0;
-    /**
-     * @var int
-     */
-    public const FORMAT_ZIP = 1;
+class InvoiceGet extends AbstractType implements TypeInterface {
+	/**
+	 * @var int
+	 */
+	public const FORMAT_CSV = 0;
+	/**
+	 * @var int
+	 */
+	public const FORMAT_ZIP = 1;
 
-    /**
-     * @var int
-     */
-    public const STATIONARY_INCLUDE = 0;
-    /**
-     * @var int
-     */
-    public const STATIONARY_EXCLUDE = 1;
+	/**
+	 * @var int
+	 */
+	public const STATIONARY_INCLUDE = 0;
+	/**
+	 * @var int
+	 */
+	public const STATIONARY_EXCLUDE = 1;
 
-    /**
-     * @var array
-     */
-    protected $template = [
-        'type_identifier' => 'INVOICE_GET',
-        'invoice_id' => null,
-        'client_id' => null,
-        'customer_id' => null,
-        'invoice_date_from' => null,
-        'invoice_date_to' => null,
-        'sent_only' => null,
-        'format' => null,
-        'changed_only' => null,
-        'system_name' => null,
-        'created_by_system_only' => null,
-        'stationary_exclude' => null,
-        'output_required' => null,
-    ];
+	/**
+	 * @var array
+	 */
+	protected $template = [
+		'type_identifier'        => 'INVOICE_GET',
+		'invoice_id'             => null,
+		'client_id'              => null,
+		'customer_id'            => null,
+		'invoice_date_from'      => null,
+		'invoice_date_to'        => null,
+		'sent_only'              => null,
+		'format'                 => null,
+		'changed_only'           => null,
+		'system_name'            => null,
+		'created_by_system_only' => null,
+		'stationary_exclude'     => null,
+		'output_required'        => null,
+		'order_id'               => null,
+		'product_id'             => null
+	];
 
-    /**
-     * Formally validates the type data in $data attribute.
-     *
-     * @return bool Validation success
-     */
-    public function validate(): bool
-    {
-        return true;
-    }
+	/**
+	 * Formally validates the type data in $data attribute.
+	 *
+	 * @return bool Validation success
+	 */
+	public function validate(): bool {
+		return true;
+	}
 }
